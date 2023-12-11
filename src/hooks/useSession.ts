0078288998c230365c507a42ecad9e4b392dae0c
jevvonnift <jevon.christian@iftgroup.id>
2023-12-11 20:31:58 +0700

    change router refresh to window location reload

--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -1,18 +1,16 @@
 "use client";
 
 import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { api } from "~/trpc/react";
 
 const useSession = () => {
   const { data: session, isLoading, isError } = api.auth.session.useQuery();
-  const router = useRouter();
 
   useEffect(() => {
     if (isError) {
       Cookies.remove("token");
-      router.refresh();
+      window.location.reload();
     }
   }, [isError]);
 
